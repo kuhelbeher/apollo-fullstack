1a7cdef68c212025973c10d0722e92d4d41f281d
Bartenev Dmitriy <kuhelbeher@gmail.com>
2019-09-01 10:51:45 +0300

    Setup yoga server

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,1 +1,20 @@
-// let's go!
+require('dotenv').config();
+const createServer = require('./createServer');
+const db = require('./db');
+
+const server = createServer();
+
+// TODO: user express middleware to handle cookies
+// TODO: use express middleware to populate current user
+
+server.start(
+  {
+    cors: {
+      credentials: true,
+      origin: process.env.FRONTEND_URL,
+    },
+  },
+  deets => {
+    console.log(`Server is running on port http://localhost:${deets.port}`);
+  },
+);
