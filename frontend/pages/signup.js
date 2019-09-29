2bed00b555177a9af1cd2791fe5fa0aac3336ab2
Bartenev Dmitriy <kuhelbeher@gmail.com>
2019-09-29 18:46:31 +0300

    Added user signup

---
+++ frontend/pages/signup.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import styled from 'styled-components';
+
+import Signup from '../components/Signup';
+
+const Columns = styled.div`
+  display: grid;
+  grid-template-columns: repeat(auto-fit, minmax(300px, 1fr));
+  grid-gap: 20px;
+`;
+
+function SignupPage() {
+  return (
+    <Columns>
+      <Signup />
+      <Signup />
+      <Signup />
+    </Columns>
+  );
+}
+
+export default SignupPage;
